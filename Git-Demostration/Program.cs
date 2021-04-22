34100a5e971e27e04d1b1fcf4336c990d0a6025b
Legrain Samuel <samuel.legrain@bstorm.be>
2021-04-22 10:19:51 +0200

    Ajoutez des fichiers projet.

---
+++ Git-Demostration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_Demostration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("New project!");
+        }
+    }
+}
